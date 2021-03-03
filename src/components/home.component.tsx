4c9bc1be62133dc343a18c998f53882db4313c6b
Keith Tuttle <klktj01@gmail.com>
2021-03-03 02:03:29 -0500

    Work on Register page and hook up front/back end via axios
    work in progress - will still need validation and to update the user model to have passwords

--- src/components/home.component.tsx
+++ src/components/home.component.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default class Home extends Component {
     render(){
         return(
-            <div>Home Page!</div>
+            <div className="container">Home Page!</div>
         );
     }
 }
