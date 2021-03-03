6100bb0b2a6fde3f52f1d0ae27edca50e384226c
Keith Tuttle <klktj01@gmail.com>
2021-03-03 00:47:29 -0500

    started creating front end routes and components. Import Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar.component";
+import Home from "./components/home.component";
+import RegisterUser from "./components/register-user.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Map'Pin Template!</p>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={Home} />
+      <Route path="/user" exact component={RegisterUser} />
+    </Router>
   );
 }
 
---
+++ src/components/register-user.component.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class RegisterUser extends Component {
+    render(){
+        return(
+            <div>Register User</div>
+        );
+    }
+}
