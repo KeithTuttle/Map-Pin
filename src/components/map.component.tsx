ec77131bb37c8174b97ab6cafbaae71e8275741a
speaco3 <speaco3@students.towson.edu>
2021-03-09 17:54:54 -0500

    added temporary marker functionality

--- src/components/map.component.tsx
+++ src/components/map.component.tsx
@@ -1,5 +1,19 @@
+import { render } from '@testing-library/react';
 import * as React from 'react';
-import ReactMapGL, { NavigationControl, Marker } from 'react-map-gl';
+import { OverlayTrigger, Popover } from 'react-bootstrap';
+import ReactMapGL, { NavigationControl, Marker, MapEvent } from 'react-map-gl';
+import { MouseEvent } from 'react-mapbox-gl/lib/geojson-layer';
+
+const popover = (latitude: number, longitude: number) => (
+    <Popover id="popover-basic">
+      <Popover.Title as="h3">Popover bottom</Popover.Title>
+      <Popover.Content>
+        Latitude: {latitude.toFixed(2)}
+        <br />
+        Longitude: {longitude.toFixed(2)}
+      </Popover.Content>
+    </Popover>
+);
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN || '';
 const initialState = {
@@ -10,9 +24,29 @@ const initialState = {
         width: 400,
         zoom: 14,
     },
+    markers: [
+        <OverlayTrigger rootClose trigger="click" placement="bottom" overlay={popover(39.406, -76.610)}>
+            <Marker latitude={39.406} longitude={-76.610} offsetLeft={-20} offsetTop={-10}>
+                <img className="imageHover" src="/assets/pin.jpg" alt="Here" style={{width: 35, height: 35}}/>
+            </Marker>
+        </OverlayTrigger>
+    ],
 };
 type State = typeof initialState;
 type Viewport = typeof initialState.viewport;
+
+const imageClicked = () => {
+    alert("image Clicked!");
+}
+
+const markerPrompt = (
+    <Popover id="popover-basic">
+        <Popover.Title as="h3">Add a new Pin!</Popover.Title>
+        <Popover.Content>
+            Here is where you can add a new pin!
+        </Popover.Content>
+    </Popover>
+)
 
 export default class Map extends React.Component<{}, State> {
     public state: State = initialState;
@@ -42,22 +76,46 @@ export default class Map extends React.Component<{}, State> {
         }));
     };
 
+    public addMarker = (event: MapEvent) => {
+        this.setState( prevState => ({
+            markers: prevState.markers.concat(
+                <OverlayTrigger trigger="click" placement="bottom" overlay={popover(event.lngLat[1], event.lngLat[0])}>
+                    <Marker latitude={event.lngLat[1]} longitude={event.lngLat[0]} offsetLeft={-20} offsetTop={-10}>
+                        <img className="imageHover" src="/assets/pin.jpg" alt="Here" style={{width: 35, height: 35}} /*onClick={() => 
+                            //alert('latitude: ' + event.lngLat[1] + '\nlongitude: ' + event.lngLat[0])
+                            //this.deleteMarker([event.lngLat[1], event.lngLat[0]])
+                        }*/
+                        />
+                    </Marker>
+                </OverlayTrigger>
+                )
+            })
+        )
+        this.state.markers.forEach(marker => console.log(marker.props.latitude + ' , ' + marker.props.longitude))
+    }
+
+    public deleteMarker = (arr: number[]) => {
+        let index: number = -1;
+        this.state.markers.forEach( (marker, i) => {
+            if(marker.props.latitude === arr[0] && marker.props.longitude === arr[1])
+                index = i;
+        })
+        this.setState( prevState => ({
+            markers: prevState.markers.splice(index, 1)
+        }))
+    }
     
     public render() {
         const { viewport } = this.state;
-        const imageClicked = () => {
-            alert("image Clicked!");
-        }
         return (
             <ReactMapGL
                 {...viewport}
                 className="map-size"
                 mapboxApiAccessToken={MAPBOX_TOKEN}
                 onViewportChange={(v: Viewport) => this.updateViewport(v)}
+                onClick={(event: MapEvent) => this.addMarker(event)}
             >
-                <Marker latitude={39.4018552} longitude={-76.602388} offsetLeft={-20} offsetTop={-10}>
-                    <img className="imageHover" src="/assets/pin.jpg" alt="Here" style={{width: 35, height: 35}} onClick={() => imageClicked()} />
-                </Marker>
+                {this.state.markers}
                 <div>
                     <NavigationControl onViewportChange={this.updateViewport} />
                 </div>
---
+++ src/components/markerPrompt.component.tsx
@@ -0,0 +1,2 @@
+import React, { useState } from 'react';
+import { Popover } from 'react-bootstrap';
