a7c3ef804192ba2647470154759570b336b8460c
Keith Tuttle <klktj01@gmail.com>
2021-04-01 19:01:07 -0400

    update navbar style

--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default class Navbar extends Component {
     render(){
         return(
-            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-                <Link to="/" className="navbar-brand">MapPin</Link>
+            <nav className="container topnav navbar navbar-dark bg-dark navbar-expand-lg">
+                <NavLink to="/" className="main_title navbar-brand">MapPin</NavLink>
                 <div className="collpase navbar-collapse">
                     <ul className="navbar-nav ml-auto">
                         <li className="navbar-item">
-                            <Link to="/register" className="nav-link">Register</Link>
+                            <NavLink style={{color: 'white'}} to="/register" className="nav-link">Register</NavLink>
                         </li>
                         <li className="navbar-item">
-                            <Link to="/login" className="nav-link">Log-In</Link>
+                            <NavLink style={{color: 'white'}} to="/login" className="nav-link">Log-In</NavLink>
                         </li>
                     </ul>
                 </div>
