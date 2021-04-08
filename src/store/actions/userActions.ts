8e18d37b324de251d4ac5c7facf7b28a85b46ee5
Keith Tuttle <klktj01@gmail.com>
2021-04-08 15:50:08 -0400

    separate the frontend in its own folder

--- src/store/actions/userActions.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "../../viewModels/UserWithErrorMessage";
-import dispatcher from "../dispatcher";
-
-export function setUser(user: User) {
-    dispatcher.dispatch({
-        type: "SET_USER",
-        user,
-    });
-}
