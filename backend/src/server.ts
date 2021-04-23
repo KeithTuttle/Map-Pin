4e254f3e4c94600dffaebfeafb1f4c767969e2b8
Keith Tuttle <klktj01@gmail.com>
2021-04-22 22:57:18 -0400

    more deployment testing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -29,10 +29,10 @@ const main = async () => {
     app.use('/users', usersRouter);
 
     if (process.env.NODE_ENV === 'production'){
-      app.use(express.static('frontend/build'));
+      app.use(express.static('public'));
 
       app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname + '/public/index.html'));
       })
     }
 
